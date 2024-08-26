d2e90148dd72a06d1e593c6b7ebbfbf3a7f4043e
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-26 17:29:46 -0400

    se creo la entidad de course

---
+++ src/main/java/com/spring/graphql/entities/Course.java
@@ -0,0 +1,25 @@
+package com.spring.graphql.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
+@Getter
+@Setter
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private String teacher;
+
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY, cascade = CascadeType.ALL, targetEntity = Student.class)
+    private List<Student> studentList;
+}
