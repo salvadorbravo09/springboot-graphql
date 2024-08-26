42202930aebd4926d8035acc3d2f4bfde780823a
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-26 17:37:18 -0400

    se agrego la interfaz de courserepository para las operaciones crud basicas

---
+++ src/main/java/com/spring/graphql/repositories/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.spring.graphql.repositories;
+
+import com.spring.graphql.entities.Course;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends CrudRepository<Course, Long> {
+}
