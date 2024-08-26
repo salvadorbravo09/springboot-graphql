bfb05a59557681415aae4d8670109b3af6f79d27
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-26 17:12:28 -0400

    se agrego la entidad estudiante

---
+++ src/main/java/com/spring/graphql/entities/Student.java
@@ -0,0 +1,23 @@
+package com.spring.graphql.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private Integer age;
+}
