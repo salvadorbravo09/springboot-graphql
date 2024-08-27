081f3ac75da51bc2571752ec7abe5e627e5f2a3d
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-27 13:51:24 -0400

    se agregaron datos sql de ejemplo

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+-- Insertar datos en la tabla de cursos
+INSERT INTO courses (name, category, teacher) VALUES ('Matemáticas', 'Ciencias Exactas', 'Ana María Soto');
+INSERT INTO courses (name, category, teacher) VALUES ('Biología', 'Ciencias Naturales', 'Carlos Fernández');
+INSERT INTO courses (name, category, teacher) VALUES ('Programación', 'Tecnología', 'Gabriel Morales');
+
+-- Insertar datos en la tabla de estudiantes
+INSERT INTO students (name, last_name, age, course_id) VALUES ('Juan', 'Pérez', '26', 1);
+INSERT INTO students (name, last_name, age, course_id) VALUES ('Isabel', 'López', '21', 2);
+INSERT INTO students (name, last_name, age, course_id) VALUES ('María', 'González', '22', 1);
+INSERT INTO students (name, last_name, age, course_id) VALUES ('Diego', 'Molina', '24', 3);
